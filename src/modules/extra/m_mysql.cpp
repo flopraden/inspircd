61490ac29d022c93c4fc84c7cfebfb276663c9f3
Florian Praden <florian@praden.eu>
2015-02-02 15:25:24 +0100

    * Add characterset option to MySQL database connection

--- src/modules/extra/m_mysql.cpp
+++ src/modules/extra/m_mysql.cpp
@@ -255,12 +255,15 @@ class SQLConnection : public SQLProvider
 		bool rv = mysql_real_connect(connection, host.c_str(), user.c_str(), pass.c_str(), dbname.c_str(), port, NULL, 0);
 		if (!rv)
 			return rv;
-
-		// Enable character set settings
-		std::string charset = config->getString("charset");
-		if ((!charset.empty()) && (mysql_set_character_set(connection, charset.c_str())))
-			ServerInstance->Logs->Log(MODNAME, LOG_DEFAULT, "WARNING: Could not set character set to \"%s\"", charset.c_str());
-
+		// enable character-set settings
+		std::string characterset;
+		if (config->readString("characterset", characterset))
+		{
+		  if (!mysql_set_character_set(connection,characterset.c_str()))
+		  {
+		    ServerInstance->Logs->Log(MODNAME, LOG_DEFAULT, "WARNING: Could not change character-set to " + characterset);
+		  }
+		}
 		std::string initquery;
 		if (config->readString("initialquery", initquery))
 		{
